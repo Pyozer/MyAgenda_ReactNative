a8aecb4c4c47db304cc7b8ae81176750e9a5152f
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-07-11 00:30:13 +0200

    Amélioration du bouton Menu

--- components/pages/MainScreen.js
+++ components/pages/MainScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { TouchableOpacity, Text, Button } from "react-native";
 import { createDrawerNavigator } from "react-navigation";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import Container from "../ui/View/Container";
@@ -9,12 +9,13 @@ export default class MainScreen extends React.Component {
     return {
       title: "MyAgenda",
       headerLeft: (
-        <Icon
-          name="menu"
-          size={30}
-          onPress={() => navigation.toggleDrawer()}
-          style={{ padding: 16 }}
-        />
+        <TouchableOpacity
+          onPress={() => {
+            navigation.openDrawer();
+          }}
+        >
+          <Icon name="menu" size={30} style={{ padding: 16 }} />
+        </TouchableOpacity>
       )
     };
   };
